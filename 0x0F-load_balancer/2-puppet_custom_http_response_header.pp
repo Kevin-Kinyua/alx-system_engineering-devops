6c50e7c911eb441d6fd580a07def5b38a00f8aa5
Kevin-Kinyua <w.kevinkinyua@gmail.com>
2023-07-03 02:30:51 -0700

    Task 2: Add a custom HTTP header with Puppet Just as in task #0, we’d like you to automate the task of creating a custom HTTP header response, but with Puppet.
    The name of the custom HTTP header must be X-Served-By
    The value of the custom HTTP header must be the hostname of the server Nginx is running on
    Write 2-puppet_custom_http_response_header.pp so that it configures a brand new Ubuntu machine to the requirements asked in this task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,38 +1,16 @@
-# Script that setup a nginx web server on our server + redirection + header
-exec { 'update':
+# Use Puppet to automate the task of creating a custom HTTP header response
+
+exec {'update':
   command => '/usr/bin/apt-get update',
 }
-
-package { 'nginx':
-  ensure  => installed,
-  name    => 'nginx',
-  require => Exec['update'],
+-> package {'nginx':
+  ensure => 'present',
 }
-
-file { '/var/www/html/index.html':
-  ensure  => 'present',
-  path    => '/var/www/html/index.html',
-  content => 'Holberton School',
-  require => Package['nginx'],
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-file_line { 'redirect_me':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  require => Package['nginx'],
-}
-
-file_line { 'addHeader':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By $hostname;',
-  require => Package['nginx'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
